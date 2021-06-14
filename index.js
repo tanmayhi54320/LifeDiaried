1dfe42861aab9ac9d04e75af1db404b728ec8794
tanmayhi54320 <tanmayhi54320@hotmail.com>
2021-06-14 19:27:28 +0530

    Server

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
+const app =express();
+dotenv.config();
+
+app.use(bodyParser.json({limit:"30mb",extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
+
+app.use(cors());
+app.use('/posts',postRoutes);
+
+// const CONNECTION_URL='mongodb+srv://tanmay1310:8400626922@cluster0.abrmi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const PORT=process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL,{ useNewUrlParser:true , useUnifiedTopology:true})
+    .then(()=>app.listen(PORT,()=>console.log(`Server running on port ${PORT}`)))
+    .catch((error)=>console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
+
+
+
